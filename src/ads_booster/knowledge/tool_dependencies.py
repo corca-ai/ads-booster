124fef66684c99ceb3a295b0fb4e50abdbd1c79c
ParkJoowon <qkrwndnjs7308@gmail.com>
2026-09-09 09:43:09 +0900

    refactor: extend knowledge tool dependencies (#148)

--- src/ads_booster/knowledge/tool_dependencies.py
+++ src/ads_booster/knowledge/tool_dependencies.py
@@ -7,10 +7,12 @@ if TYPE_CHECKING:
     from ads_booster.knowledge.change_publication import ChangePublisher
     from ads_booster.knowledge.corrections import KnowledgeCorrections
     from ads_booster.knowledge.ingestion import KnowledgeIngestion
+    from ads_booster.knowledge.legacy_memory import LegacyMemoryGuard
     from ads_booster.knowledge.questions import KnowledgeQuestions
     from ads_booster.knowledge.repository import SqliteKnowledgeRepository
     from ads_booster.knowledge.repository_tool_state import RepositoryToolState
     from ads_booster.knowledge.retrieval import KnowledgeRetriever
+    from ads_booster.knowledge.skills import KnowledgeSkills
     from ads_booster.knowledge.web_search import SourceSearch
 
 
@@ -24,6 +26,8 @@ class ToolDependencies:
     source_search: SourceSearch | None
     questions: KnowledgeQuestions
     corrections: KnowledgeCorrections
+    skills: KnowledgeSkills
+    legacy_memory: LegacyMemoryGuard | None
 
 
 __all__ = ["ToolDependencies"]
